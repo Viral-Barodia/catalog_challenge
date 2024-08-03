3fd80a99c1458f8edd433d0ec259538653d7abfa
Viral-Barodia <202001007@daiict.ac.in>
2024-08-03 22:35:46 +0530

    solving CORS issue

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: 'https://query1.finance.yahoo.com/v8/finance/chart/NVDA'
+    apiUrl: 'https://cors-anywhere.herokuapp.com/https://query1.finance.yahoo.com/v8/finance/chart/NVDA'
 }
